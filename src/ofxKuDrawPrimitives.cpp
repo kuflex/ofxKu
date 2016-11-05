8ed82e5b067b70a52c630b878dfe18c492a9a469
perevalovds <perevalovds@gmail.com>
2016-11-05 20:51:46 +0500

    draw primitives

---
+++ src/ofxKuDrawPrimitives.cpp
@@ -0,0 +1,436 @@
+﻿#include "ofxKuDrawPrimitives.h"
+
+//--------------------------------------------------------------
+//огрубление с шагом size
+float coarse( float x, float size ) {
+    return int( x / size + 0.5 ) * size;
+}
+
+//--------------------------------------------------------------
+//1, -1 равновероятно
+int randomSign() {
+    return ( ofRandom( 0, 1 ) < 0.5 ) ? 1 : (-1);
+}
+
+//--------------------------------------------------------------
+float sqr( float x ) {
+    return x * x;
+}
+
+
+//--------------------------------------------------------------
+ofxKuRectRender::ofxKuRectRender() {
+	N = 0;
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::clear() {
+	N = 0;
+	points.clear();
+	colors.clear();
+	texs.clear();
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::start() {	//starts drawing, not clear
+	N = 0;
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::allocate_colored(int n) {
+	points.resize(n*4);
+	colors.resize(n*4);
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::allocate_textured(int n) {
+	points.resize(n*4);
+	texs.resize(n*4);
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::allocate_colored_textured(int n) {
+	points.resize(n*4);
+	colors.resize(n*4);
+	texs.resize(n*4);
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::draw_colored() {
+	if ( N == 0 ) { return; }
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    
+    glVertexPointer( 3, GL_FLOAT, sizeof( ofPoint ), &points[0].x);
+    glColorPointer( 4, GL_UNSIGNED_BYTE, sizeof( ofColor ), &colors[0].v[0]);
+    
+    glDrawArrays(GL_QUADS, 0, N);
+    
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::draw_textured( ofTexture &texture ) {
+	if ( N == 0 ) { return; }
+   
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+    glVertexPointer( 3, GL_FLOAT, sizeof( ofPoint ), &points[0].x);
+    glTexCoordPointer( 2, GL_FLOAT, sizeof( ofVec2f ), &texs[0].x );
+    
+    texture.bind();
+    glDrawArrays(GL_QUADS, 0, N);
+    texture.unbind();
+		
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::draw_colored_textured( ofTexture &texture ) {
+	if ( N == 0 ) { return; }
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    
+    glVertexPointer( 3, GL_FLOAT, sizeof( ofPoint ), &points[0].x);
+    glColorPointer( 4, GL_UNSIGNED_BYTE, sizeof( ofColor ), &colors[0].v[0]);
+    glTexCoordPointer( 2, GL_FLOAT, sizeof( ofVec2f ), &texs[0].x );
+    
+    texture.bind();
+    glDrawArrays(GL_QUADS, 0, N);
+    texture.unbind();
+    
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//-------------------------------------------------------------- 
+void ofxKuRectRender::check_size_colored() {
+	if (points.size() < N+4) points.resize(N+4);
+	if (colors.size() < N+4) colors.resize(N+4);
+}
+
+//-------------------------------------------------------------- 
+void ofxKuRectRender::check_size_textured() {
+	if (points.size() < N+4) points.resize(N+4);
+	if (texs.size() < N+4) texs.resize(N+4);
+
+}
+
+//-------------------------------------------------------------- 
+void ofxKuRectRender::check_size_colored_textured() {
+	if (points.size() < N+4) points.resize(N+4);
+	if (colors.size() < N+4) colors.resize(N+4);
+	if (texs.size() < N+4) texs.resize(N+4);
+}
+
+//-------------------------------------------------------------- 
+void ofxKuRectRender::pushRect( float x, float y, float w, float h, const ofColor &color )
+{
+	check_size_colored();
+    points[N] = ofPoint( x, y );
+    colors[N++] = color;
+    points[N] = ofPoint( x + w, y );
+    colors[N++] = color;
+    points[N] = ofPoint( x + w, y + h );
+    colors[N++] = color;
+    points[N] = ofPoint( x, y + h );
+    colors[N++] = color;
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( float x, float y, float z, float w, float h, const ofColor &color )
+{
+	check_size_colored();
+    points[N] = ofPoint( x, y, z );
+    colors[N++] = color;
+    points[N] = ofPoint( x + w, y, z );
+    colors[N++] = color;
+    points[N] = ofPoint( x + w, y + h, z );
+    colors[N++] = color;
+    points[N] = ofPoint( x, y + h, z );
+    colors[N++] = color;
+}
+
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h, const ofColor &color )
+{
+	check_size_colored();
+    points[N] = p;
+    colors[N++] = color;
+    points[N] = p + w;
+    colors[N++] = color;
+    points[N] = p + w + h;
+    colors[N++] = color;
+    points[N] = p + h;
+    colors[N++] = color;
+}
+
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h,
+                const ofRectangle &texRect )
+{
+	check_size_textured();
+    points[N] = p;
+    texs[N++] = texRect.getTopLeft();
+    points[N] = p + w;
+    texs[N++] = texRect.getTopRight();
+    points[N] = p + w + h;
+    texs[N++] = texRect.getBottomRight();
+    points[N] = p + h;
+    texs[N++] = texRect.getBottomLeft();
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( float x, float y, float z, float w, float h,
+                const ofRectangle &texRect )
+{
+	check_size_textured();
+    points[N] = ofPoint( x, y, z );
+    texs[N++] = texRect.getTopLeft();
+    points[N] = ofPoint( x + w, y, z );
+    texs[N++] = texRect.getTopRight();
+    points[N] = ofPoint( x + w, y + h, z );
+    texs[N++] = texRect.getBottomRight();
+    points[N] = ofPoint( x, y + h, z );
+    texs[N++] = texRect.getBottomLeft();    
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( const ofPoint &p1, const ofPoint &p2, const ofPoint &p3, const ofPoint &p4,
+                const ofRectangle &texRect ) 
+{
+	check_size_textured();
+    points[N] = p1;
+    texs[N++] = texRect.getTopLeft();
+    points[N] = p2;
+    texs[N++] = texRect.getTopRight();
+    points[N] = p3);
+    texs[N++] = texRect.getBottomRight();
+    points[N] = p4;
+    texs[N++] = texRect.getBottomLeft();     
+}
+
+//--------------------------------------------------------------
+//текстурированные прямоугольники с цветом
+void ofxKuRectRender::pushRect( float x, float y, float z, float w, float h,
+                const ofColor &color, const ofRectangle &texRect )
+{
+	check_size_colored_textured();
+    points[N] = ofPoint( x, y, z );
+	colors[N] = color;
+    texs[N++] = texRect.getTopLeft();
+    points[N] = ofPoint( x + w, y, z );
+	colors[N] = color;
+    texs[N++] = texRect.getTopRight();
+    points[N] = ofPoint( x + w, y + h, z );
+	colors[N] = color;
+    texs[N++] = texRect.getBottomRight();
+    points[N] = ofPoint( x, y + h, z );
+	colors[N] = color;
+    texs[N++] = texRect.getBottomLeft();    
+}
+
+//--------------------------------------------------------------
+void ofxKuRectRender::pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h,
+                const ofColor &color, const ofRectangle &texRect )
+{
+	check_size_colored_textured();
+    points[N] = p;
+	colors[N] = color;
+    texs[N++] = texRect.getTopLeft();
+    points[N] = p + w;
+	colors[N] = color;
+    texs[N++] = texRect.getTopRight();
+    points[N] = p + w + h;
+	colors[N] = color;
+    texs[N++] = texRect.getBottomRight();
+    points[N] = p + h;
+	colors[N] = color;
+    texs[N++] = texRect.getBottomLeft();
+}
+
+
+//----------------------------------------------------------
+void kuStartSmoothing(){
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+	glEnable(GL_LINE_SMOOTH);
+    
+	//why do we need this?
+	//glEnable(GL_BLEND);
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+
+//----------------------------------------------------------
+void kuEndSmoothing(){
+    glDisable(GL_LINE_SMOOTH);
+}
+
+//--------------------------------------------------------------
+//цветные линии
+void kuPushLine( const ofPoint &p1, const ofPoint &p2, const ofColor &color,
+                vector<ofPoint> &points, vector<ofColor> &colors )
+{
+    points.push_back( p1 );
+    points.push_back( p2 );
+    
+    colors.push_back( color );
+    colors.push_back( color );
+}
+
+
+void kuPushLine( const ofPoint &p1, const ofPoint &p2,
+                const ofColor &color1, const ofColor &color2,
+                vector<ofPoint> &points, vector<ofColor> &colors )
+{
+    points.push_back( p1 );
+    points.push_back( p2 );
+    
+    colors.push_back( color1 );
+    colors.push_back( color2 );
+}
+
+//--------------------------------------------------------------
+void kuDrawLines( vector<ofPoint> &points, vector<ofColor> &colors )
+{
+    if ( points.empty() ) { return; }
+
+    kuStartSmoothing();     //включаем сглаживание
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    
+    glVertexPointer( 3, GL_FLOAT, sizeof( ofPoint ), &points[0].x);
+    glColorPointer( 4, GL_UNSIGNED_BYTE, sizeof( ofColor ), &colors[0].v[0]);
+    
+    glDrawArrays(GL_LINES, 0, points.size());
+    
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    
+    //kuEndSmoothing();       //выключаем сглаживание
+
+}
+
+//--------------------------------------------------------------
+//предполагается, что текстура уже подключена
+void kuDrawLines_textured( vector<ofPoint> &points,
+                          vector<ofVec2f> &texs ) {
+    if ( points.empty() ) { return; }
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    
+    glVertexPointer( 3, GL_FLOAT, sizeof( ofPoint ), &points[0].x);
+    glTexCoordPointer( 2, GL_FLOAT, sizeof( ofVec2f ), &texs[0].x );
+    
+    //texture.bind();
+    glDrawArrays(GL_LINES, 0, points.size());
+    //texture.unbind();
+    
+    glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+//--------------------------------------------------------------
+
+void ofxKuVertexRender::setup( string pointImageFile, string shaderVert, string shaderFrag )
+{
+    ofDisableArbTex();
+	ofLoadImage( texture, pointImageFile );
+	ofEnableArbTex();
+
+    useShader_ = (shaderVert!="" && shaderFrag!="");
+    if ( useShader_ ) {
+        shader_.load( shaderVert, shaderFrag );
+    }
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::clearPoints()
+{
+    points.clear();
+    sizes.clear();
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::addPoint( const ofPoint &p, float rad, float alpha )
+{
+    points.push_back( p );
+    sizes.push_back( ofPoint( rad, alpha, 0 ) );
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::setPoints( vector<ofPoint> &v, float minRad, float maxRad ) {
+    points = v;
+    int n = v.size();
+    sizes.resize( n );
+    for (int i=0; i<n; i++) {
+        sizes[i] = ofPoint( ofRandom( minRad, maxRad ), 1, 0 );
+    }
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::setPoints( vector<ofPoint> &v, vector<ofPoint> &data ) {
+    points = v;
+    sizes = data;
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::drawBegin( float part ) {
+    //начало рисования, можно установить параметры в шейдер
+    if ( points.empty() ) { return; }
+    part = ofClamp( part, 0, 1 );
+    int total = (int)points.size() * part;
+    if ( total == 0 ) return;
+    
+	vbo.setVertexData(&points[0], total, GL_STATIC_DRAW);
+	vbo.setNormalData(&sizes[0], total, GL_STATIC_DRAW);
+	
+    //	ofEnableBlendMode(OF_BLENDMODE_ADD);
+	ofEnablePointSprites();
+
+    if ( useShader_ ) {
+        shader_.begin();
+    }
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::drawEnd( float part ){ //конец рисования
+    if ( points.empty() ) { return; }
+    part = ofClamp( part, 0, 1 );
+    int total = (int)points.size() * part;
+    if ( total == 0 ) return;
+    
+    texture.bind();
+	vbo.draw(GL_POINTS, 0, total);
+	texture.unbind();
+	
+    if ( useShader_ ) {
+        shader_.end();
+    }
+	
+	ofDisablePointSprites();
+	//ofDisableBlendMode();
+    
+    //    ofEnableAlphaBlending();
+}
+
+//--------------------------------------------------------------
+void ofxKuVertexRender::draw()
+{
+	drawBegin();
+    drawEnd();
+	
+}
+    
+//--------------------------------------------------------------
+
---
+++ src/ofxKuDrawPrimitives.h
@@ -0,0 +1,121 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+//Fast drawing
+
+//--------------------------------------------------------------
+//coarsing with step с шагом size
+float coarse( float x, float size );
+
+//1, -1
+int randomSign();
+
+float sqr( float x );
+//--------------------------------------------------------------
+
+class ofxKuRectRender {
+	ofxKuRectRender();
+	void clear();
+	void start();	//starts drawing, not clear
+	void allocate_colored(int n);		//do allocate is size is known, and use start
+	void allocate_textured(int n);
+	void allocate_colored_textured(int n);
+
+	void draw_colored();
+	void draw_textured( ofTexture &texture );
+	void draw_colored_textured( ofTexture &texture );
+	
+	//Colored rectangles
+	void pushRect( float x, float y, float w, float h, const ofColor &color );
+	void pushRect( float x, float y, float z, float w, float h, const ofColor &color );
+	void pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h, const ofColor &color );
+	
+
+	//Textured rectangles
+	void pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h, const ofRectangle &texRect );
+	void pushRect( float x, float y, float z, float w, float h, const ofRectangle &texRect );
+	void pushRect( const ofPoint &p1, const ofPoint &p2, const ofPoint &p3, const ofPoint &p4, const ofRectangle &texRect );
+
+	//Colored textured rectangles
+	void pushRect( float x, float y, float z, float w, float h,
+                const ofColor &color, const ofRectangle &texRect );
+
+	void pushRect( const ofPoint &p, const ofPoint &w, const ofPoint &h,
+                const ofColor &color, const ofRectangle &texRect );
+
+	void pushRect( vector<ofPoint> &points, vector<ofColor> &colors,
+                 vector<ofVec2f> &texs, ofTexture &texture );
+
+	vector<ofPoint> points;
+	vector<ofColor> colors;
+	vector<ofVec2f> texs;
+	int N;
+
+private:
+	void check_size_colored();
+	void check_size_textured();
+	void check_size_colored_textured();
+};
+
+
+void ofxKuStartSmoothing();    //Line smoothing
+void ofxKuEndSmoothing();
+
+class ofxKuLineRender {
+	void clear();
+	void allocate_colored(int n);
+	void allocate_textured(int n);
+	void allocate_colored_textured(int n);
+
+	void draw_colored();
+	void draw_textured( ofTexture &texture );
+	void draw_colored_textured( ofTexture &texture );
+
+	void pushLine( const ofPoint &p1, const ofPoint &p2, const ofColor &color );
+	void pushLine( const ofPoint &p1, const ofPoint &p2, const ofColor &color1,
+                const ofColor &color2 );
+	void pushLine( vector<ofPoint> &points, vector<ofColor> &colors );
+
+};
+
+
+
+
+
+//Вершины, рисуются текстурой
+//Внимание: сами используют шейдеры, поэтому не должны вызываться при включенном шейдере
+class ofxKuVertexRender {
+public:
+    void setup( string pointImageFile, string shaderVert = "", string shaderFrag = "" );
+    //если оставить пути к шейдеру пустыми - то нужно самим подключать шейдер,
+    //после drawBegin и выключать после drawEnd
+    
+    void clearPoints();
+    void addPoint( const ofPoint &p, float rad, float alpha );
+    void setPoints( vector<ofPoint> &v, float minRad, float maxRad );
+    
+    //data - данные, которые передаются в шейдер как нормаль
+    //например, data.x - size, data.y,z - текстурные координаты
+    void setPoints( vector<ofPoint> &v, vector<ofPoint> &data );
+    
+    void draw();
+    
+    void drawBegin( float part = 1 );   //начало рисования, можно установить параметры в шейдер
+    //part - доля сколько частиц рисовать, 0..1
+    ofShader &shader() { return shader_; }
+    void drawEnd( float part = 1 ); //конец рисования, part должен совпадать с началом
+    
+private:
+    vector <ofVec3f> points;
+    vector <ofVec3f> sizes;
+    
+    ofTexture texture;
+    bool useShader_;
+    ofShader shader_;
+    ofVbo vbo;
+    
+};
+
+
+
