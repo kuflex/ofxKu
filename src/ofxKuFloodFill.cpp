c32001c8c6197ab2088427383fcf7e182a78e175
Denis Perevalov <perevalovds@gmail.com>
2017-02-08 20:49:28 +0500

    Add files via upload

---
+++ src/ofxKuFloodFill.cpp
@@ -0,0 +1,56 @@
+﻿#include "ofxKuFloodFill.h"
+
+//--------------------------------------------------------------
+
+size_t kuFloodFill( vector<unsigned char> &input, int w, int h, int sv,
+                 int x0, int y0, int search, int fillColor, vector<int> *outPoints ) {
+    vector<int> res;
+    res.push_back(x0 + w*y0);
+    input[x0 + w*y0] = fillColor;
+
+	vector<int> rose(sv);
+	rose[0] = 1;
+	rose[1] = -1;
+	rose[2] = w;
+	rose[3] = -w;
+	if (sv == 8) {
+		rose[4] = 1 + w;
+		rose[5] = 1 - w;
+		rose[6] = -1 + w;
+		rose[7] = -1 - w;
+	}
+
+
+    size_t begin = 0;
+    while (begin < res.size()) {
+        int p = res[begin];
+        //int px = p%w;
+        //int py = p/h;
+
+        begin++;
+		for (int i=0; i<sv; i++) {
+			int q = p+rose[i];
+			if (q>=0 && q<w*h && input[q] == search) {
+				res.push_back(q);
+				input[q] = fillColor;
+			}
+		}
+
+        /*for (int y = py - 1; y <= py + 1; y++)  {
+            for (int x = px - 1; x <= px + 1; x++) {
+                if (x >= 0 && x < w && y >= 0 && y < h
+                        && (sv==8 || x == px || y == py)
+                        && input( x y ) == search ) {
+                    res.push_back( int2(x,y) );
+                    input.pixel( x, y ) = fillColor;
+                }
+
+            }
+        }*/
+    }
+
+    if ( outPoints ) { *outPoints = res; }
+    return res.size();
+}
+//--------------------------------------------------------------
+
---
+++ src/ofxKuFloodFill.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+//Flood fill and blobs processing
+
+//--------------------------------------------------------------
+//Flood fill
+//sv=4 or 8 - connectivity of pixels
+//outPoints as x+w*y
+void ofxKuFloodFill(vector<unsigned char> &input, int w, int h, int sv,
+                 int x0, int y0, int search, int fillColor, vector<int> *outPoints); 
+
+//--------------------------------------------------------------
