afcaf78da2bb4678683a9c7752a80b7edf48f8fb
Denis Perevalov <perevalovds@gmail.com>
2017-04-14 13:31:46 +0500

    working with file times

---
+++ src/ofxKuFileTime.cpp
@@ -0,0 +1,33 @@
+#include "ofxKuFileTime.h"
+
+//--------------------------------------------------------------
+bool ofxKuFileTime::isNewerThan(string fileName1, string fileName2) {
+	if (ofFile::doesFileExist(fileName1) && ofFile::doesFileExist(fileName2)) {
+		ofFile file1(fileName1, ofFile::ReadOnly, true);
+		ofFile file2(fileName2, ofFile::ReadOnly, true);
+
+		Poco::Timestamp created1 = file1.getPocoFile().created();
+		Poco::Timestamp created2 = file2.getPocoFile().created();
+		return (created1 > created2);
+	}
+	return false;
+}
+
+//--------------------------------------------------------------
+/*bool Filetime_is_file_older_hours(string fileName, int hours) {
+	ofFile file(fileName, ofFile::ReadOnly, true);
+	Poco::Timestamp created = file.getPocoFile().created();
+	Poco::Timestamp now = Poco::Timestamp::Timestamp();
+
+	std::time_t created_sec = created.epochTime();
+	std::time_t now_sec = now.epochTime();
+
+	//cout << "created " << created_sec << " now " << now_sec << endl;
+	//cout << "time_diff " << (now_sec - created_sec)/(60*60) << endl;
+	//if (now_sec > created_sec + 60*60*hours) cout << "OLD" << endl;
+
+	return (now_sec > created_sec + 60*60*hours);
+	//return (created.isElapsed(hours*60*60*1000000));
+}
+*/
+//--------------------------------------------------------------
---
+++ src/ofxKuFileTime.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//Analyzing file creation time
+//Currently uses POCO, so works for oF 0.8.4, not oF 0.9.X
+
+#include "ofMain.h"
+
+struct ofxKuFileTime {
+	bool isNewerThan(string fileName1, string fileName2);
+};
+//bool Filetime_is_file_older_hours(string fileName, int hours);
