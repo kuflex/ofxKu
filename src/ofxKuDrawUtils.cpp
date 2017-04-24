53553d22b8a5d79d5cc1a18f9d0b3b18f66de46f
Denis Perevalov <perevalovds@gmail.com>
2017-04-24 15:09:26 +0300

    Add files via upload

---
+++ src/ofxKuDrawUtils.cpp
@@ -0,0 +1,16 @@
+﻿#include "ofxKuDrawUtils.h"
+
+
+//--------------------------------------------------------------
+void ofxKuDrawTextureFit(ofTexture &tex, float x, float y, float w, float h) {
+	float tw = tex.getWidth();
+	float th = tex.getHeight();
+	if (tw>0 && th>0) {
+		float scl = min(w/tw,h/th);
+		tw *= scl;
+		th *= scl;
+		tex.draw(x+w/2-tw/2, y+h/2-th/2, tw, th);
+	}
+}
+//--------------------------------------------------------------
+
---
+++ src/ofxKuDrawUtils.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+//Drawing utils
+
+void ofxKuDrawTextureFit(ofTexture &tex, float x, float y, float w, float h);
