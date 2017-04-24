a2f843eaedcceb8781bc26b0e8ed6f46483df9ad
Denis Perevalov <perevalovds@gmail.com>
2017-04-24 15:07:58 +0300

    draw raster arrays

---
+++ src/ofxKuDrawRasters.cpp
@@ -0,0 +1,21 @@
+﻿#include "ofxKuDrawRasters.h"
+
+//--------------------------------------------------------------
+void ofxKuDrawRaster(vector<unsigned char> &pix, int w, int h, float X, float Y, float W, float H) {
+	if (pix.size() != w*h) return;
+	ofTexture texture;
+	texture.loadData(&pix[0],w,h,GL_LUMINANCE);
+	texture.draw(X,Y,W,H);
+}
+
+//--------------------------------------------------------------
+void ofxKuDrawRaster(vector<float> &pix, int w, int h, float X, float Y, float W, float H, float val0, float val1) {
+	if (pix.size() != w*h) return;
+	vector<unsigned char> pix1(w*h);
+	for (int i=0; i<w*h; i++) {
+		pix1[i] = ofMap(pix[i],val0,val1,0,255);
+	}
+	ofxKuDrawRaster(pix1, w, h, X, Y, W, H);
+}
+
+//--------------------------------------------------------------
---
+++ src/ofxKuDrawRasters.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+//Drawing rasters
+
+void ofxKuDrawRaster(vector<unsigned char> &pix, int w, int h, float X, float Y, float W, float H);
+void ofxKuDrawRaster(vector<float> &pix, int w, int h, float X, float Y, float W, float H, float val0=0, float val1=255);
