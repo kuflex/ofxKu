309008c02df115697d0e7de9e563981ca71edfcc
Denis Perevalov <perevalovds@gmail.com>
2018-02-11 09:36:03 +0500

    added smooth functions

---
+++ src/ofxKuValueSmooth.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//Smoothing
+
+#include "ofMain.h"
+
+template <typename T>
+void ofxKuValueSmooth(T &value, T target, float smooth) {
+    value += (target - value) * smooth;
+}
+
+
+template <typename T>
+void ofxKuValueSmoothDirected(T &value, T target, float smooth0, float smooth1) {
+	float smooth = (target < value) ? smooth0 : smooth1;
+	ofxKuValueSmooth(value, target, smooth);
+}
