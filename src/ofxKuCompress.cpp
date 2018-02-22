8ac9b33bc048ffa7e5d8c80392b3dd76d169a80d
perevalovds <perevalovds@gmail.com>
2018-02-23 01:02:47 +0500

    added compression depth data

--- src/ofxKuCompress.cpp
+++ src/ofxKuCompress.cpp
@@ -1,7 +1,7 @@
 #include "ofxKuCompress.h"
 
 //--------------------------------------------------------------
-//RLE compression
+//RLE compression for masks - zero values are compressed
 //if v==0, then 0,count(0..255)
 //if v=1..255, then just v
 void ofxKuCompressRle(vector<unsigned char> &dataIn, vector<unsigned char> &dataOut) {
@@ -53,6 +53,71 @@ void ofxKuDecompressRle(vector<unsigned char> &dataIn, vector<unsigned char> &da
 			}
 		}
 		for (int j=0; j<fill_count && k<size; j++) {
+			dataOut[k++] = fill_value;
+		}
+	}
+}
+
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+//RLE compression for depth data - zero values are compressed
+//if v==0, then 0,count(0..65535)
+//if v=1..65535, then just v
+void ofxKuCompressRle(unsigned short *dataIn, int size, vector<unsigned short> &dataOut) {
+	int n = size;
+	dataOut.reserve(n);
+	int zero = 0;
+	for (int i = 0; i<n; i++) {
+		unsigned short &v = dataIn[i];
+		if (zero>0 && (zero == 65535 || v>0)) {
+			dataOut.push_back(zero);
+			zero = 0;
+		}
+		if (v == 0) {
+			if (zero == 0) dataOut.push_back(0);
+			zero++;
+		}
+		else {
+			dataOut.push_back(v);
+		}
+	}
+	if (zero>0) dataOut.push_back(zero);
+}
+
+//--------------------------------------------------------------
+void ofxKuCompressRle(vector<unsigned short> &dataIn, vector<unsigned short> &dataOut) {
+	ofxKuCompressRle(&dataIn[0], dataIn.size(), dataOut);
+}
+
+//--------------------------------------------------------------
+//RLE decompression
+//size - expected size
+void ofxKuDecompressRle(vector<unsigned short> &dataIn, vector<unsigned short> &dataOut, int size) {
+	dataOut.resize(size);
+	int n = dataIn.size();
+	int k = 0;
+	int state_zero = 0;
+	unsigned short fill_value = 0;
+	unsigned short fill_count = 0;
+	for (int i = 0; i<n; i++) {
+		unsigned short &v = dataIn[i];
+		if (state_zero) {
+			state_zero = 0;
+			fill_value = 0;
+			fill_count = v;
+		}
+		else {
+			if (v == 0) {
+				state_zero = 1;
+				fill_count = 0;
+			}
+			else {
+				fill_value = v;
+				fill_count = 1;
+			}
+		}
+		for (int j = 0; j<fill_count && k<size; j++) {
 			dataOut[k++] = fill_value;
 		}
 	}
--- src/ofxKuCompress.h
+++ src/ofxKuCompress.h
@@ -4,7 +4,7 @@
 
 #include "ofMain.h"
 
-//RLE compression
+//RLE compression for masks - zero values are compressed
 //if v==0, then 0,count(0..255)
 //if v=1..255, then just v
 void ofxKuCompressRle(vector<unsigned char> &dataIn, vector<unsigned char> &dataOut);
@@ -13,4 +13,13 @@ void ofxKuCompressRle(vector<unsigned char> &dataIn, vector<unsigned char> &data
 //size - expected size
 void ofxKuDecompressRle(vector<unsigned char> &dataIn, vector<unsigned char> &dataOut, int size);
 
+//RLE compression for depth data - zero values are compressed
+//if v==0, then 0,count(0..65535)
+//if v=1..65535, then just v
+void ofxKuCompressRle(unsigned short *dataIn, int size, vector<unsigned short> &dataOut);
+void ofxKuCompressRle(vector<unsigned short> &dataIn, vector<unsigned short> &dataOut);
+
+//RLE decompression
+//size - expected size
+void ofxKuDecompressRle(vector<unsigned short> &dataIn, vector<unsigned short> &dataOut, int size);
 
