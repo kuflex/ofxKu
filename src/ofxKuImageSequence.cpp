bf4693fc3f5ae47f93025ba6ffaa6c98993430ca
perevalovds <perevalovds@gmail.com>
2018-08-29 13:13:44 +0500

    img seq

---
+++ src/ofxKuImageSequence.cpp
@@ -0,0 +1,48 @@
+﻿#include "ofxKuImageSequence.h"
+
+//--------------------------------------------------------------
+void ofxKuImageSequence::load(string folder) {
+	ofDirectory dir;
+	int n = dir.listDir(folder);
+	tex_.resize(n);
+	for (int i = 0; i < n; i++) {
+		ofLoadImage(tex_[i], dir.getPath(i));
+	}
+
+}
+
+//--------------------------------------------------------------
+void ofxKuImageSequence::setAnchorPercent(float x, float y) {
+	for (int i = 0; i < tex_.size(); i++) {
+		tex_[i].setAnchorPercent(x, y);
+	}
+}
+
+//--------------------------------------------------------------
+int ofxKuImageSequence::size() {
+	return tex_.size();
+}
+
+//--------------------------------------------------------------
+ofTexture &ofxKuImageSequence::get_texture(int i) {
+	if (i >= 0 && i < size()) {
+		return tex_[i];
+	}
+	else {
+		if (!empty_.isAllocated()) {
+			empty_.allocate(1, 1, GL_RGB);
+		}
+		return empty_;
+	}
+}
+
+//--------------------------------------------------------------
+void ofxKuImageSequence::draw(int i, float x, float y, float w, float h) {
+	if (i >= 0 && i < size()) {
+		if (w == 0 && h == 0) tex_[i].draw(x, y);
+		else tex_[i].draw(x, y, w, h);
+	}
+}
+
+//--------------------------------------------------------------
+
---
+++ src/ofxKuImageSequence.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+//Draw image sequence
+
+struct ofxKuImageSequence {
+	void load(string folder);
+	int size();
+	ofTexture &get_texture(int i);
+	vector<ofTexture> &textures() { return tex_; }
+
+	void draw(int i, float x, float y, float w = 0, float h = 0);
+	void setAnchorPercent(float x, float y);
+
+protected:
+	vector<ofTexture> tex_;
+	ofTexture empty_;
+
+};
